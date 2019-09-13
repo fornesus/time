643a67974c0d08b87469f6677c62b293393f27aa
Chris Fornesa <admin@cftxp.net>
2019-09-12 20:22:02 -0500

    Re-do PWA configurations
    This commit redoes PWA configurations.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'; // Import from the React DOM
 import './index.css'; // The main stylesheet
 import App from './App';  // Import the main App component
 import Footer from './Footer';  // Import the Footer component
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 // Render the main App class React application component as a child element of the <div id="#root"> HTML element:
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -14,4 +14,4 @@ ReactDOM.render(<Footer />, document.querySelector('footer'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
